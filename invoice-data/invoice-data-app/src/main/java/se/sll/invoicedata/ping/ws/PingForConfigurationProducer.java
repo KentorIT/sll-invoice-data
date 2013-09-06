c9dad7047017dde7b8d174ee4aa6bd8baf9a7b5b
Peter Larsson <advptr@gmail.com>
2013-09-06 11:25:44 +0200

    First draft of maven project.

---
+++ invoice-data/invoice-data-app/src/main/java/se/sll/invoicedata/ping/ws/PingForConfigurationProducer.java
@@ -0,0 +1,53 @@
+/**
+ *  Copyright (c) 2013 SLL <http://sll.se/>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package se.sll.invoicedata.ping.ws;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import se.riv.itintegration.monitoring.rivtabp21.v1.PingForConfigurationResponderInterface;
+import se.riv.itintegration.monitoring.v1.PingForConfigurationResponseType;
+import se.riv.itintegration.monitoring.v1.PingForConfigurationType;
+
+@WebService(serviceName = "PingForConfiguationProducer", endpointInterface = "se.riv.itintegration.monitoring.rivtabp21.v1.PingForConfigurationResponderInterface", portName = "PingForConfigurationResponderPort", targetNamespace = "urn:riv:itintegration:monitoring:PingForConfigurationResponder:1:rivtabp21")
+public class PingForConfigurationProducer implements PingForConfigurationResponderInterface {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(PingForConfigurationProducer.class);
+    
+    
+    @WebResult(name = "PingForConfigurationResponse", targetNamespace = "urn:riv:itintegration:monitoring:PingForConfigurationResponder:1", partName = "parameters")
+    @WebMethod(operationName = "PingForConfiguration", action = "urn:riv:itintegration:monitoring:PingForConfigurationResponder:1:PingForConfiguration")
+    public PingForConfigurationResponseType pingForConfiguration(
+            @WebParam(partName = "LogicalAddress", name = "LogicalAddress", targetNamespace = "urn:riv:itintegration:registry:1", header = true) String arg0,
+            @WebParam(partName = "parameters", name = "PingForConfiguration", targetNamespace = "urn:riv:itintegration:monitoring:PingForConfigurationResponder:1") PingForConfigurationType arg1) {
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        PingForConfigurationResponseType response = new PingForConfigurationResponseType();
+        response.setVersion("1.0");
+        response.setPingDateTime(formatter.format(new Date()));
+        return response;
+    }
+}
---
+++ invoice-data/invoice-data-app/src/main/java/se/sll/invoicedata/ws/AbstractProducer.java
@@ -0,0 +1,65 @@
+/**
+ *  Copyright (c) 2013 SLL <http://sll.se/>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package se.sll.invoicedata.ws;
+
+import java.util.Map;
+import java.util.UUID;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import org.apache.cxf.binding.soap.SoapFault;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Abstract class used by WS Producers
+ *
+ * @author Albert Örwall
+ * @author Khaled Daham
+ *
+ */
+public abstract class AbstractProducer {
+
+    private final Logger logger = LoggerFactory.getLogger("pdl");
+
+    protected void throwInternalServerError(Throwable t) {
+        String errorText = "Internal server error (reference: " + UUID.randomUUID().toString() + ")";
+        logger.error(errorText, t);
+        throw new SoapFault(errorText, SoapFault.FAULT_CODE_SERVER);
+    }
+
+    /**
+     *
+     * @param wsctx
+     * @param msg
+     */
+    protected void logPDL(WebServiceContext wsctx, String msg) {
+        MessageContext mctx = null;
+        if (wsctx != null) {
+            mctx = wsctx.getMessageContext();
+        } else {
+            logger.error("wsctx is null");
+            return;
+        }
+        Map http_headers = (Map) mctx.get(MessageContext.HTTP_REQUEST_HEADERS);
+        if (http_headers.containsKey("x-rivta-original-serviceconsumer-hsaid")) {
+            logger.info("{} invoked by HSAid: {}", msg, http_headers.get("x-rivta-original-serviceconsumer-hsaid"));
+        } else {
+            logger.info("{} invoked by HSAid: NOT FOUND.", msg);
+        }
+    }
+}
