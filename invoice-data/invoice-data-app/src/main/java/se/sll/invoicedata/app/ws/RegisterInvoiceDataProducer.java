eda7d5b4ea439b631976ff2481fbd7e4ca2d50cc
Peter Larsson <advptr@gmail.com>
2013-09-06 16:34:27 +0200

    Cleaned-up stuff.

--- invoice-data/invoice-data-app/src/main/java/se/sll/invoicedata/app/ws/RegisterInvoiceDataProducer.java
+++ invoice-data/invoice-data-app/src/main/java/se/sll/invoicedata/app/ws/RegisterInvoiceDataProducer.java
@@ -16,37 +16,26 @@
 
 package se.sll.invoicedata.app.ws;
 
-import javax.annotation.Resource;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
-import javax.jws.WebService;
-import javax.xml.ws.WebServiceContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import riv.sll.invoicedata.registerinvoicedata._1.rivtabp21.RegisterInvoiceDataResponderInterface;
 import riv.sll.invoicedata.registerinvoicedataresponder._1.RegisterInvoiceDataResponseType;
 import riv.sll.invoicedata.registerinvoicedataresponder._1.ObjectFactory;
+import riv.sll.invoicedata.registerinvoicedataresponder._1.RegisterInvoiceDataType;
 import riv.sll.invoicedata._1.ResultCode;
 import riv.sll.invoicedata._1.ResultCodeEnumType;
 
 public class RegisterInvoiceDataProducer extends AbstractProducer implements RegisterInvoiceDataResponderInterface {
 
     private static final Logger LOG = LoggerFactory.getLogger(RegisterInvoiceDataProducer.class);
-    @Resource
-    WebServiceContext wsctx;
+//    @Resource
+//    WebServiceContext wsctx;
             
       
     @Override      
-    public RegisterInvoiceDataResponseType registerInvoiceData(
-        @WebParam(partName = "LogicalAddress", name = "LogicalAddress", targetNamespace = "urn:riv:itintegration:registry:1", header = true)
-        java.lang.String logicalAddress,
-        @WebParam(partName = "parameters", name = "RegisterInvoiceData", targetNamespace = "urn:riv:sll:invoicedata:RegisterInvoiceDataResponder:1")
-        riv.sll.invoicedata.registerinvoicedataresponder._1.RegisterInvoiceDataType parameters
-    ) {
+    public RegisterInvoiceDataResponseType registerInvoiceData(String logicalAddress, RegisterInvoiceDataType parameters) {
     	ObjectFactory f = new ObjectFactory();
     	RegisterInvoiceDataResponseType ur = f.createRegisterInvoiceDataResponseType();
     	
