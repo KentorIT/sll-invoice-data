ab65a0bd90452f01762497aa2d71339def80e4a6
Muqeet Khan <khanmuqeetkhan@gmail.com>
2013-10-02 09:24:08 +0200

    Added ViewInvoiceData as a service; added tests on it

---
+++ invoice-data/invoice-data-app/src/test/java/se/sll/invoicedata/app/ws/ViewInvoiceDataProducerTest.java
@@ -0,0 +1,130 @@
+/**
+ *  Copyright (c) 2013 SLL <http://sll.se/>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ * 
+ */
+package se.sll.invoicedata.app.ws;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import riv.sll.invoicedata._1.Event;
+import riv.sll.invoicedata._1.RegisteredEvent;
+import riv.sll.invoicedata._1.ResultCodeEnum;
+import riv.sll.invoicedata.createinvoicedataresponder._1.CreateInvoiceDataRequest;
+import riv.sll.invoicedata.createinvoicedataresponder._1.CreateInvoiceDataResponse;
+import riv.sll.invoicedata.getinvoicedataresponder._1.GetInvoiceDataRequest;
+import riv.sll.invoicedata.getinvoicedataresponder._1.GetInvoiceDataResponse;
+import riv.sll.invoicedata.registerinvoicedataresponder._1.RegisterInvoiceDataResponse;
+import riv.sll.invoicedata.viewinvoicedata._1.rivtabp21.ViewInvoiceDataResponderInterface;
+import riv.sll.invoicedata.viewinvoicedataresponder._1.ViewInvoiceDataRequest;
+import riv.sll.invoicedata.viewinvoicedataresponder._1.ViewInvoiceDataResponse;
+import se.sll.invoicedata.app.TestSupport;
+
+/**
+ * @author muqkha
+ *
+ */
+public class ViewInvoiceDataProducerTest extends TestSupport {
+
+	private final String LOGICAL_ADDRESS = "loc:TolkPortalen";
+	
+	//@Test
+	public void viewInvoiceData_Success() {
+		
+		Event event = createSampleEventData();
+		RegisterInvoiceDataResponse regIDResp = RegisterInvoiceDataProducerTest.
+				getRegisterInvoiceDataService().registerInvoiceData(LOGICAL_ADDRESS, event);
+		
+		Assert.assertNotNull(regIDResp);
+		Assert.assertEquals(ResultCodeEnum.OK, regIDResp.getResultCode().getCode());
+		
+		GetInvoiceDataRequest getIDReq = new GetInvoiceDataRequest();
+		getIDReq.setSupplierId(event.getSupplierId());
+		getIDReq.setPaymentResponsible(event.getPaymentResponsible());
+		
+		GetInvoiceDataResponse invoiceResp = GetInvoiceDataProducerTest.
+				getGetInvoiceDataService().getInvoiceData(LOGICAL_ADDRESS, getIDReq);
+		
+		Assert.assertNotNull(invoiceResp);
+		Assert.assertNotNull(invoiceResp.getRegisteredEventList());
+		
+		List<RegisteredEvent> regEventList = invoiceResp.getRegisteredEventList();
+
+		List<Long> eventRefList = new ArrayList<Long>();
+		for (RegisteredEvent regEvent : regEventList) {
+			eventRefList.add(regEvent.getId());
+		}
+
+		CreateInvoiceDataRequest invoiceDataRequest = new CreateInvoiceDataRequest();
+		invoiceDataRequest.setSupplierId(event.getSupplierId());
+		invoiceDataRequest.setPaymentResponsible(event.getPaymentResponsible());
+		invoiceDataRequest.setCreatedBy("test");
+		invoiceDataRequest.getEventRefIdList().addAll(eventRefList);
+
+		CreateInvoiceDataResponse createIDResp = CreateInvoiceDataProducerTest.
+				getCreateInvoiceDataService().createInvoiceData(LOGICAL_ADDRESS, invoiceDataRequest);
+		
+		Assert.assertNotNull(createIDResp.getReferenceId());
+		
+		ViewInvoiceDataRequest viewIDReq = new ViewInvoiceDataRequest();
+		viewIDReq.setReferenceId(createIDResp.getReferenceId());
+		
+		ViewInvoiceDataResponse viewIDResp = getViewInvoiceDataService().viewInvoiceData(LOGICAL_ADDRESS, viewIDReq);
+		
+		Assert.assertNotNull(viewIDResp);
+		Assert.assertNotNull(viewIDResp.getInvoiceData());
+		Assert.assertNotNull(viewIDResp.getInvoiceData().getEventList());
+		Assert.assertEquals(ResultCodeEnum.OK, viewIDResp.getResultCode().getCode());
+		
+		Assert.assertEquals(event.getSupplierId(), viewIDResp.getInvoiceData().getSupplierId());
+	}
+	
+	
+	private ViewInvoiceDataResponderInterface getViewInvoiceDataService() {
+		ViewInvoiceDataResponderInterface iGetInvoiceDataResponder = null;
+		final String URL = "http://localhost:8080/invoice-data-app/ws/viewInvoiceData";
+		
+		try {
+			URL wsdlURL = new URL(URL + "?wsdl");
+
+			String namespaceURI = "http://ws.app.invoicedata.sll.se/";
+			String serviceName = "ViewInvoiceDataProducerService";
+
+			QName serviceQN = new QName(namespaceURI, serviceName);
+
+			Service service = Service.create(wsdlURL, serviceQN);
+
+			iGetInvoiceDataResponder = service
+					.getPort(ViewInvoiceDataResponderInterface.class);
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return iGetInvoiceDataResponder;
+	}
+}
