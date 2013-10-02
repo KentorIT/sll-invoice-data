88e808c5ed79342fbc4edf3f12e3bfa4d6fdd951
Peter Larsson <advptr@gmail.com>
2013-10-02 17:44:06 +0200

    Aligned ViewInvoiceDataProducer to new pattern

--- invoice-data/invoice-data-app/src/main/java/se/sll/invoicedata/app/ws/ViewInvoiceDataProducer.java
+++ invoice-data/invoice-data-app/src/main/java/se/sll/invoicedata/app/ws/ViewInvoiceDataProducer.java
@@ -19,18 +19,10 @@
  */
 package se.sll.invoicedata.app.ws;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import riv.sll.invoicedata._1.ResultCode;
-import riv.sll.invoicedata._1.ResultCodeEnum;
 import riv.sll.invoicedata.viewinvoicedata._1.rivtabp21.ViewInvoiceDataResponderInterface;
 import riv.sll.invoicedata.viewinvoicedataresponder._1.ObjectFactory;
 import riv.sll.invoicedata.viewinvoicedataresponder._1.ViewInvoiceDataRequest;
 import riv.sll.invoicedata.viewinvoicedataresponder._1.ViewInvoiceDataResponse;
-import se.sll.invoicedata.core.service.InvoiceDataService;
-import se.sll.invoicedata.core.service.InvoiceDataServiceException;
 
 
 
@@ -38,36 +30,25 @@ import se.sll.invoicedata.core.service.InvoiceDataServiceException;
  * @author muqkha
  * 
  */
-public class ViewInvoiceDataProducer extends AbstractProducer implements
-		ViewInvoiceDataResponderInterface {
+public class ViewInvoiceDataProducer extends AbstractProducer implements ViewInvoiceDataResponderInterface {
 
-	private static final Logger log = LoggerFactory.getLogger(ViewInvoiceDataProducer.class);
-
-	@Autowired
-	private InvoiceDataService invoiceDataService;
+    static final ObjectFactory factory = new ObjectFactory();
 
 	/**
 	 * 
 	 */
 	@Override
-	public ViewInvoiceDataResponse viewInvoiceData(String logicalAddress,
-			ViewInvoiceDataRequest parameters) {		
-		log("viewInvoiceData");
-        log.info("logicalAddress: {}", logicalAddress);
+	public ViewInvoiceDataResponse viewInvoiceData(final String logicalAddress,
+			final ViewInvoiceDataRequest parameters) {		
 		
-        final ObjectFactory oFactory = new ObjectFactory();
-        final ResultCode rc = new ResultCode();
-        final ViewInvoiceDataResponse viewIDataResponse = oFactory.createViewInvoiceDataResponse();
-        
-        try {
-        	viewIDataResponse.setInvoiceData(invoiceDataService.getInvoiceDataByReferenceId(parameters.getReferenceId()));
-        } catch (InvoiceDataServiceException ex) {
-            rc.setCode(ResultCodeEnum.ERROR);
-            rc.setMessage(ex.getMessage());
-            log.error(ex.getMessage());
-        }
-        rc.setCode(ResultCodeEnum.OK);
-        viewIDataResponse.setResultCode(rc);
+        final ViewInvoiceDataResponse viewIDataResponse = factory.createViewInvoiceDataResponse();
+
+        viewIDataResponse.setResultCode(invoke(new Runnable() {
+            @Override
+            public void run() {
+                viewIDataResponse.setInvoiceData(getInvoiceDataService().getInvoiceDataByReferenceId(parameters.getReferenceId()));
+            }
+        }));
         return viewIDataResponse;
 	}
 
