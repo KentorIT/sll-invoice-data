2154cda4ba166cbf380c3d0bd166a8db0fadbd72
Peter Larsson <advptr@gmail.com>
2013-10-09 16:14:16 +0200

    Added more tests on price lists.
    Former-commit-id: 6109dfcdec9dfe0842f68d1bcc10b5651a3f67f3

--- invoice-data/invoice-data-core/src/test/java/se/sll/invoicedata/core/service/impl/PriceListServiceImplTest.java
+++ invoice-data/invoice-data-core/src/test/java/se/sll/invoicedata/core/service/impl/PriceListServiceImplTest.java
@@ -22,8 +22,10 @@ import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Collections;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import se.sll.invoicedata.core.service.PriceListService;
 import se.sll.invoicedata.core.service.dto.Price;
@@ -35,8 +37,65 @@ public class PriceListServiceImplTest extends TestSupport {
     @Autowired
     private PriceListService priceListService;
     
+    @Before
+    public void deleteAll() {
+        for (final PriceList priceList : priceListService.getPriceLists()) {
+            priceListService.deletePriceList(priceList.getId());
+        }
+    }
+    
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testSave_null_valid_from_date() {
+        final PriceList priceList = createSamplePriceList();
+        priceList.setValidFrom(null);
+        priceListService.savePriceLists(Collections.singletonList(priceList));        
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testSave_null_supplier_id() {
+        final PriceList priceList = createSamplePriceList();
+        priceList.setSupplierId(null);
+        priceListService.savePriceLists(Collections.singletonList(priceList));        
+    }
+    
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testSave_null_service_code() {
+        final PriceList priceList = createSamplePriceList();
+        priceList.setServiceCode(null);
+        priceListService.savePriceLists(Collections.singletonList(priceList));        
+    }
+    
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testSave_null_item_id() {
+        final PriceList priceList = createSamplePriceList();
+        priceList.getPrices().get(0).setItemId(null);
+        priceListService.savePriceLists(Collections.singletonList(priceList));        
+    }    
+    
+
     @Test
     public void testSave_success() {
+        final PriceList priceList = createSamplePriceList();
+        
+        priceListService.savePriceLists(Collections.singletonList(priceList));
+        
+        assertEquals(1, priceListService.getPriceLists().size());        
+    }
+    
+    
+    
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testSave_duplicate_item_id_fail() {
+        final PriceList priceList = createSamplePriceList();
+        
+        priceList.getPrices().get(0).setItemId("item.1");
+        priceList.getPrices().get(1).setItemId("item.1");
+        
+        priceListService.savePriceList(priceList);        
+    }
+    
+    
+    PriceList createSamplePriceList() {
         final PriceList priceList = new PriceList();
         
         priceList.setSupplierId("Tolk.001");
@@ -50,13 +109,11 @@ public class PriceListServiceImplTest extends TestSupport {
         Price p2 = new Price();
         p2.setItemId("item.2");
         p2.setPrice(BigDecimal.valueOf(650.00));
-        
+
         priceList.getPrices().add(p1);
         priceList.getPrices().add(p2);
-        
-        priceListService.savePriceLists(Collections.singletonList(priceList));
-        
-        assertEquals(1, priceListService.getPriceLists().size());        
+
+        return priceList;
     }
     
     
