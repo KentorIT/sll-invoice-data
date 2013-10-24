255d728c816349f37921fb8ef5fabc371e8652f2
Peter Larsson <advptr@gmail.com>
2013-10-24 19:56:49 +0200

    Updated stats with history lengh and possibility to reset stats.
    Former-commit-id: 874d3b4de9252f9532c1050090815a781b414d9d

--- invoice-data/invoice-data-core/src/main/java/se/sll/invoicedata/core/jmx/StatusBean.java
+++ invoice-data/invoice-data-core/src/main/java/se/sll/invoicedata/core/jmx/StatusBean.java
@@ -19,6 +19,8 @@
 
 package se.sll.invoicedata.core.jmx;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -29,8 +31,10 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedMetric;
 import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedOperationParameter;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.jmx.support.MetricType;
 import org.springframework.stereotype.Component;
@@ -55,6 +59,9 @@ public class StatusBean {
     @Autowired
     private InvoiceDataService invoiceDataService;
 
+    //
+    private int historyLength = 1000;
+    
     //
     private static ThreadLocal<Stack<Sample>> samples = new ThreadLocal<Stack<Sample>>() {
         @Override
@@ -122,14 +129,34 @@ public class StatusBean {
         return (avg / n);
     }
 
-    @ManagedOperation(description="Returns performance metrics in millisceonds for all active WSDL operations")
-    public Properties getPerformanceMetrics() {
-        final Properties props = new Properties();
-        for (Map.Entry<String, Timer> entry : timerMap.entrySet()) {
-            props.setProperty(entry.getKey(), entry.getValue().toString());
+    @ManagedOperation(description="Returns performance metrics (JSON strings) in millisceonds for all instrumented operations")
+    public String[] getPerformanceMetrics() {
+        Collection<Timer> c = timerMap.values();
+        final String[] list = new String[c.size()];
+        int i = 0;
+        for (Timer t : c) {
+            list[i++] = t.toString();
         }
-        return props;
+        return list;
     }
+
+    @ManagedOperation(description="Clears sampled performance metrics (timed statistics)")
+    public void clearPerformanceMetrics() {
+        timerMap.clear();
+    }
+
+    @ManagedAttribute(description="Sets the length of request history for timed statistics", defaultValue="1000")
+    public void setHistoryLength(@ManagedOperationParameter(name="historyLength", description="Indicates history of requests to keep/calculate averge timed statistcis") int historyLength) {
+        if (historyLength > 0) {
+            this.historyLength = historyLength;
+        }
+    }
+
+    @ManagedAttribute(description="Returns the length of request history for timed statistics")
+    public int getHistoryLength() {
+        return historyLength;
+    }
+
 
     @ManagedOperation(description="Returns active service contract names, i.e. WSDL operations that have been accessed since last startup")
     public String[] getServiceNames() {
@@ -149,10 +176,15 @@ public class StatusBean {
 
     //
     public void start(final String path) {
+        start(path, false);
+    }
+    
+    //
+    public void start(final String path, boolean history) {
         if (samples.get().size() == 0) {
             concurrency.inc();
         }
-        samples.get().push(new Sample(path));
+        samples.get().push(new Sample(path, history));
     }
 
     //
@@ -162,7 +194,7 @@ public class StatusBean {
         final long elapsed = sample.elapsed();
         Timer timer = timerMap.get(name);
         if (timer == null) {
-            timer = new Timer();
+            timer = new HistoryTimer(name, this.historyLength);
             timerMap.put(name, timer);
         }
         timer.add(elapsed);
@@ -179,14 +211,22 @@ public class StatusBean {
         private long timestamp;
         private String name;
         private String guid;
+        private boolean history;
 
         //
-        public Sample(final String name) {
+        public Sample(final String name, boolean history) {
             this.timestamp = System.currentTimeMillis();
             this.name = name;
             this.guid = UUID.randomUUID().toString();
+            this.history = history;
         }
 
+        //
+        public boolean isHistoryEnabled() {
+            return history;
+        }
+        
+        //
         public String getGUID() {
             return guid;
         }
@@ -202,6 +242,42 @@ public class StatusBean {
             return (time < 0) ? 0 : time;
         }
     }
+    
+    static class HistoryTimer extends Timer {
+        private int len;
+        private int ofs = 0;
+        private long[] history;
+
+        //
+        public HistoryTimer(String name, int len) {
+            super(name);
+            this.len = len;
+            this.history = new long[len];
+            Arrays.fill(history, -1);
+        }
+
+        //
+        public void add(long t) {
+            if (ofs >= len) {
+                ofs = 0;
+            }
+            history[ofs++] = t;
+        }
+
+        //
+        public synchronized void recalc() {
+            reset();
+            for (int i = 0; i < len && history[i] >= 0; i++) {
+                super.add(history[i]);
+            }
+        }
+        
+        @Override
+        public synchronized String toString() {
+            recalc();
+            return String.format("{ name: \"%s\", history: %d, min: %d, max: %d, avg: %d }", name(), n(), min(), max(), avg()); 
+        }
+    }
 
     //
     static class Timer {
@@ -209,10 +285,18 @@ public class StatusBean {
         private long min;
         private long max;
         private long sum;
+        private String name;
 
-        public Timer() {
+        public Timer(String name) {
+            this.name = name;
             reset();
         }
+        
+        //
+        public String name() {
+            return name;
+        }
+
 
         //
         public void add(long t) {
@@ -252,7 +336,7 @@ public class StatusBean {
 
         @Override
         public String toString() {
-            return String.format("{ n: %d, min: %d, max: %d, avg: %d }", n(), min(), max(), avg()); 
+            return String.format("{ name: \"%s\", n: %d, min: %d, max: %d, avg: %d }", name(), n(), min(), max(), avg()); 
         }
     }
 
