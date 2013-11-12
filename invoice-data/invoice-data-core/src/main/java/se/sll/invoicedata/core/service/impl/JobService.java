398bd80549d12a3527e5d4f0a7a34f8d3a73c597
Peter Larsson <advptr@gmail.com>
2013-11-12 16:29:32 +0100

    Docs udpated and more images added.
    Former-commit-id: c63b5bc016ebe224cd24a6bbf13a40fb0063741c

---
+++ invoice-data/invoice-data-core/src/main/java/se/sll/invoicedata/core/service/impl/JobService.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2013 SLL. <http://sll.se>
+ *
+ * This file is part of Invoice-Data.
+ *
+ *     Invoice-Data is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Invoice-Data is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with Invoice-Data.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
+
+package se.sll.invoicedata.core.service.impl;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import se.sll.invoicedata.core.model.entity.InvoiceDataEntity;
+import se.sll.invoicedata.core.model.repository.InvoiceDataRepository;
+
+/**
+ * Runs service batch jobs.
+ * 
+ * @author Peter
+ *
+ */
+@Service
+public class JobService {
+
+        private static final Logger log = LoggerFactory.getLogger(JobService.class);
+
+        @Autowired
+        private InvoiceDataRepository invoiceDataRepository;
+        
+        @Value("${invoicedata.monthTTL:0}")
+        private int invoiceDataTTL;
+        
+        @Scheduled(cron="${job.cron}")
+        public void batchJob() {
+            log.info("Start batch job");
+            final Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.MONTH, (-1 * invoiceDataTTL));
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            
+            removeOldData(cal.getTime());
+        }
+        
+        @Transactional
+        private void removeOldData(final Date maxDate) {
+          if (invoiceDataTTL <= 0) {
+              log.info("Parameter invoicedata.monthTTL is zero, i.e. life-cycle management of old data is disabled");
+              return;
+          }
+          final List<InvoiceDataEntity> list = invoiceDataRepository.findByEndDateLessThan(maxDate);
+          log.info("Remove invoice data older than: {}, count: {}", maxDate, list.size());
+          invoiceDataRepository.delete(list);
+        }
+        
+}
